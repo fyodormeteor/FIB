01c1e21fd31bfe9fc924564ebb22176e38189245
Fyodor <155318006+fyodormeteor@users.noreply.github.com>
2024-04-22 17:48:26 +0200

    PRO2 Entregable 3

---
+++ PRO2/Entregables&examenes/X13400/solucio.cc
@@ -0,0 +1,71 @@
+/*  Intervals de notes dels estudiants d'un conjunt
+    22.04.2024
+
+    https://github.com/fyodormeteor
+    https://github.com/kalebgrove
+*/
+
+#include "Estudiant.hh"
+#include "Cjt_estudiants.hh"
+
+void Cjt_estudiants::afegir_estudiant(const Estudiant &est, bool& b) {
+    int i = nest-1;
+    int dni = est.consultar_DNI();
+
+    int pos = cerca_dicot(vest, 0, i, dni);
+
+    b = (dni == vest[pos].consultar_DNI());
+    
+    if(!b) {
+        while (i >= pos) {
+                vest[i+1]=vest[i];
+                --i;
+        }
+        vest[i+1] = est;
+        ++nest;
+        
+        bool has_grade = est.te_nota();
+
+        if(has_grade) {
+            incrementar_interval(est.consultar_nota());
+        }
+    } 
+}
+
+void Cjt_estudiants::esborrar_estudiant(int dni, bool& b) {
+    int pos = cerca_dicot(vest, 0, nest-1, dni);
+
+    b = (pos < nest and dni == vest[pos].consultar_DNI());
+
+    if(b) {
+
+        Estudiant est = vest[pos];
+        bool has_grade = est.te_nota();
+
+        if(has_grade) {
+            decrementar_interval(est.consultar_nota());
+        }
+
+        for(int i = pos; i < nest; i++) {
+            vest[i] = vest[i+1];
+        }
+        nest--;
+    }
+
+}
+
+void Cjt_estudiants::incrementar_interval(double x) {
+    int grade = x;  //We eliminate the decimals to place it into the interval.
+
+    if(grade == 10) grade--;
+    
+    intervals[grade]++;
+}
+
+void Cjt_estudiants::decrementar_interval(double x) {
+    int grade = x;  //We eliminate the decimals to find the correct interval.
+
+    if(grade == 10) grade--;
+
+    intervals[grade]--;
+}
